30a3b406e04b181af05b25ed6ae17b4aef79820f
Souvlaki42 <77171255+Souvlaki42@users.noreply.github.com>
2024-06-23 17:22:34 +0300

    Constraints and buffered channels

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # 1 Billion Row Challenge
 
-## Description
-
 This is my implementation for the 1 Billion Row Challenge. \
 I know the competition was for Java initially and that it have ended, but I wanted to join the fun. \
 My implementation is in Golang/Go because, I'm much more familiar with it than I'm with Java.
@@ -10,12 +8,19 @@ My implementation is in Golang/Go because, I'm much more familiar with it than I
 
 [https://youtu.be/e_9ziFKcEhw?si=GmluAFpm5fslQdvl](https://youtu.be/e_9ziFKcEhw?si=GmluAFpm5fslQdvl) \
 [https://github.com/gunnarmorling/1brc](https://github.com/gunnarmorling/1brc) \
+[https://github.com/shraddhaag/1brc/](https://github.com/shraddhaag/1brc/)
 [https://rmoff.net/2024/01/03/1%EF%B8%8F%E2%83%A3%EF%B8%8F-1brc-in-sql-with-duckdb/](https://rmoff.net/2024/01/03/1%EF%B8%8F%E2%83%A3%EF%B8%8F-1brc-in-sql-with-duckdb/) \
 [https://mrkaran.dev/posts/1brc/](https://mrkaran.dev/posts/1brc/) \
 [https://www.morling.dev/blog/one-billion-row-challenge/](https://www.morling.dev/blog/one-billion-row-challenge/) \
 [https://www.infoq.com/news/2024/01/1brc-fast-java-processing/](https://www.infoq.com/news/2024/01/1brc-fast-java-processing/) \
 [https://ftisiot.net/posts/1brows/](https://ftisiot.net/posts/1brows/)
 
+## Constraints
+
+The temperatures are between -99.9 and 99.9 and they have exactly one fractional digit. \
+Rounding should be done using the semantics of IEEE 754 with a rounding direction 'round toward positive'. \  
+Each station name is less than or equal to 100 bytes long while they are at most 10000 unique stations in the file.
+
 ## Results
 
-Fastest run of my current program with 8 threads is 2m 15s for the 1 billion lines file.
+Fastest run of my current program is 1m 18s for the 1 billion lines file.
--- main.go
+++ main.go
@@ -20,8 +20,8 @@ type Station struct {
 	max  float64
 }
 
-func processFile(path string, threads int, chunkSize int) int {
-	ch := make(chan map[string]Station, threads)
+func processFile(path string, chunkSize int) int {
+	ch := make(chan map[string]Station, chunkSize)
 	var wg sync.WaitGroup
 
 	file, err := os.Open(path)
@@ -31,8 +31,7 @@ func processFile(path string, threads int, chunkSize int) int {
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
-
-	lines := make([]string, 0)
+	lines := make([]string, 0, chunkSize)
 
 	for {
 		line, err := reader.ReadString('\n')
@@ -46,13 +45,11 @@ func processFile(path string, threads int, chunkSize int) int {
 			}
 			break
 		}
-
 		lines = append(lines, strings.TrimSpace(line))
-
 		if len(lines) >= chunkSize {
 			wg.Add(1)
 			go processChunk(lines, ch, &wg)
-			lines = make([]string, 0)
+			lines = make([]string, 0, chunkSize)
 		}
 	}
 
@@ -61,7 +58,7 @@ func processFile(path string, threads int, chunkSize int) int {
 		close(ch)
 	}()
 
-	aggregatedStations := make(map[string]Station)
+	aggregatedStations := make(map[string]Station, 10000)
 
 	for stations := range ch {
 		for name, station := range stations {
@@ -83,7 +80,7 @@ func processFile(path string, threads int, chunkSize int) int {
 	i := 1
 	fmt.Print("{ ")
 	for name, station := range aggregatedStations {
-		fmt.Printf("%s=%.1f/%.1f/%.1f", name, math.Round(station.min*10)/10, math.Round(station.mean*10)/10, math.Round(station.max*10)/10)
+		fmt.Printf("%s=%.1f/%.1f/%.1f", name, roundTowardsPositive(station.min), roundTowardsPositive(station.mean), roundTowardsPositive(station.max))
 		if i < len(aggregatedStations) {
 			fmt.Print(", ")
 		}
@@ -97,7 +94,7 @@ func processFile(path string, threads int, chunkSize int) int {
 func processChunk(chunk []string, ch chan<- map[string]Station, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	stations := make(map[string]Station)
+	stations := make(map[string]Station, 10000)
 
 	for _, line := range chunk {
 		parts := strings.SplitN(line, ";", 2)
@@ -108,7 +105,6 @@ func processChunk(chunk []string, ch chan<- map[string]Station, wg *sync.WaitGro
 		}
 
 		station, found := stations[name]
-
 		if !found {
 			station.min = measurement
 			station.max = measurement
@@ -128,20 +124,24 @@ func processChunk(chunk []string, ch chan<- map[string]Station, wg *sync.WaitGro
 	ch <- stations
 }
 
+func roundTowardsPositive(f float64) float64 {
+	return math.Ceil(f*10) / 10
+}
+
 func main() {
 	var file string
 	var threads, chunkSize int
 
 	flag.StringVar(&file, "file", "measurements.txt", "The file to take the measurements from")
 	flag.IntVar(&threads, "threads", runtime.NumCPU(), "How many threads to spawn")
-	flag.IntVar(&chunkSize, "chunkSize", 1000000, "How many chunkSize to spawn")
+	flag.IntVar(&chunkSize, "chunkSize", 1000000, "How many lines per chunk")
 	flag.Parse()
 
 	runtime.GOMAXPROCS(threads)
 
 	start := time.Now()
 
-	uniqueStations := processFile(file, threads, chunkSize)
+	uniqueStations := processFile(file, chunkSize)
 
 	end := time.Now()
 	duration := end.Sub(start)
